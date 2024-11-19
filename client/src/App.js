b761b43e72fac4fcc251771c0ca370124f449ac2
Umut Can YILMAZ <umutcyilmaz@192.168.1.14>
2024-11-19 13:52:07 +0300

    updated files

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import OtherPage from "./OtherPage";
 import Fib from "./Fib";
-
+ 
 function App() {
   return (
     <Router>
       <div className="App">
         <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -19,12 +22,13 @@ function App() {
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
-        <div>
-          <Fib />
-        </div>
+        <Routes>
+          <Route path="/" element={<Fib />} />
+          <Route path="/otherpage" element={<OtherPage />} />
+        </Routes>
       </div>
     </Router>
   );
 }
-
+ 
 export default App;
